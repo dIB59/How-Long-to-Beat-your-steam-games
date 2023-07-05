d3425ed718ae19ec61a10f9207beacb11178f45a
dIB59 <ibrahim@ebs.net.pk>
2023-07-05 15:32:06 +0200

    init commit

---
+++ server/steamgames/src/main/java/com/steamgames/steamgames/SteamgamesApplication.java
@@ -0,0 +1,13 @@
+package com.steamgames.steamgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamgamesApplication.class, args);
+	}
+
+}
