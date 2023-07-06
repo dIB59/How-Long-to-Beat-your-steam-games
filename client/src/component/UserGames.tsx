f95e575a2d7a6ec45221cbf4804b4330140c4c75
dIB59 <ibrahim@ebs.net.pk>
2023-07-06 18:46:36 +0200

    delete works

--- client/src/component/UserGames.tsx
+++ client/src/component/UserGames.tsx
@@ -12,14 +12,27 @@ type UserGamesProps = {
 
 export const UserGames: FC<UserGamesProps> = (props) => {
   const { userGamesData, loading, fetchData, handleSort, sortOrder } = props;
-  const [hoveredRow, setHoveredRow] = useState<number | null>(null);
 
   useEffect(() => {
     fetchData();
   }, []);
-  
+
+  const [hoveredRow, setHoveredRow] = useState<number | null>(null);
+
   const handleRowHover = (index: number) => {
     setHoveredRow(index);
+  };
+
+  const handleDelete = (id: number) => {
+    console.log('Delete:', id);
+    axios.delete(`http://localhost:8080/api/users/${id}`)
+      .then(() => {
+        // Update the userGamesData state or trigger a refetch if necessary.
+        fetchData();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
 
   return (
@@ -28,27 +41,43 @@ export const UserGames: FC<UserGamesProps> = (props) => {
         <p>Loading games...</p>
       ) : userGamesData && userGamesData.length > 0 ? (
         <table className='table'>
-            <thead>
-                <tr>
-                <th><h2>ID</h2></th>
-                <th>
-                  <h2>Number of Games
+          <thead>
+            <tr>
+              <th>
+                <h2>ID</h2>
+              </th>
+              <th>
+                <h2>
+                  Number of Games
                   <button onClick={handleSort}>
-                  {sortOrder === 'asc' ? '▲' : '▼'}
-                  </button></h2>
-                </th>
-                <th><h2>Steam ID</h2></th>
-                </tr>
-            </thead>
-            <tbody>
-                {userGamesData.map(data => (
-                <tr key={data.id}>
-                    <td>{data.id}</td>
-                    <td>{data.numberOfGames}</td>
-                    <td>{data.steamId}</td>
-                </tr>
-                ))}
-            </tbody>
+                    {sortOrder === 'asc' ? '▲' : '▼'}
+                  </button>
+                </h2>
+              </th>
+              <th>
+                <h2>Steam ID</h2>
+              </th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {userGamesData.map((data, index) => (
+              <tr
+                key={data.id}
+                onMouseEnter={() => handleRowHover(index)}
+                onMouseLeave={() => handleRowHover(0)}
+              >
+                <td>{data.id}</td>
+                <td>{data.numberOfGames}</td>
+                <td>{data.steamId}</td>
+                <td>
+                  {hoveredRow === index && (
+                    <button onClick={() => handleDelete(data.id)}>Delete</button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       ) : (
         <p>No user games found.</p>
