cfe93782f0fd14a068dc09d15f47af9b970e9c4f
dIB59 <ibrahim@ebs.net.pk>
2023-07-06 13:19:40 +0200

    post from front end works to the backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,43 @@
-import { useState } from 'react'
-import './App.css'
-import { UserGames } from './component/UserGames'
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import { UserGames } from './component/UserGames';
 
 function App() {
+  const [username, setUsername] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Make the POST request using Axios
+    axios
+      .post('http://localhost:8080/api/users', { userId: username })
+      .then((response) => {
+        console.log('Success:', response.data);
+        // Handle success response here
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        // Handle error here
+      });
+  };
 
   return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
       <UserGames />
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- server/steamgames/src/main/java/com/steamgames/steamgames/model/SteamUserDataResponse.java
+++ server/steamgames/src/main/java/com/steamgames/steamgames/model/SteamUserDataResponse.java
@@ -1,9 +1,5 @@
 package com.steamgames.steamgames.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-
 public record SteamUserDataResponse(
         ResponseRecord response
 ) {
